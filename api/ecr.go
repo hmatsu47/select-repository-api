f5e0fe61f290caddf505868d4228c1b1aab45e13
hmatsu47 <hmatsu47@gmail.com>
2022-09-02 00:13:47 +0900

    取得する URI を厳密に確認する形に変更（複数のタグのうちどれを使うか？）

--- api/ecr.go
+++ api/ecr.go
@@ -6,7 +6,23 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
+	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
 )
+
+// 対象のイメージタグを検索
+func ImageTag(ids []types.ImageIdentifier, tags []string, digest string) (*string, error) {
+    for _, id := range ids {
+        for _, tag := range tags {
+            // 値の比較を可能にするために変数に代入
+            vsTag := *id.ImageTag
+            vsDigest := *id.ImageDigest
+            if vsTag == tag && vsDigest == digest {
+                return &tag, nil
+            }
+        }
+    }
+    return nil, fmt.Errorf("対象のイメージタグが見つかりません")
+}
 
 // ECR リポジトリ内イメージ一覧取得
 func ImageList(repositoryName string, registryId string, repositoryUri string) ([]Image, error) {
@@ -15,13 +31,29 @@ func ImageList(repositoryName string, registryId string, repositoryUri string) (
         return nil, fmt.Errorf("AWS（API）の認証に失敗しました : %s", err)
     }
 
+    // ページネーションさせないために最大件数を 1,000 に（実際には数十個程度の想定）
+    maxResults := int32(1000)
     ecrImagesClient := ecr.NewFromConfig(cfg)
+
+    // 一旦イメージ一覧を取得しておく
+    ecrImageIds, ierr := ecrImagesClient.ListImages(context.TODO(), &ecr.ListImagesInput{
+        RepositoryName: &repositoryName,
+        RegistryId: &registryId,
+        MaxResults: &maxResults,
+    })
+    if ierr != nil {
+        return nil, fmt.Errorf("リポジトリ（%s）のイメージ一覧の取得に失敗しました : %s", repositoryName, ierr)
+    }
+    imageIds := ecrImageIds.ImageIds
+
+    // イメージ詳細一覧を取得
     ecrImages, eerr := ecrImagesClient.DescribeImages(context.TODO(), &ecr.DescribeImagesInput{
         RepositoryName: &repositoryName,
         RegistryId: &registryId,
+        MaxResults: &maxResults,
     })
     if eerr != nil {
-        return nil, fmt.Errorf("リポジトリ（%s）のイメージ一覧の取得に失敗しました : %s", repositoryName, eerr)
+        return nil, fmt.Errorf("リポジトリ（%s）のイメージ詳細一覧の取得に失敗しました : %s", repositoryName, eerr)
     }
 
     imageDetails := ecrImages.ImageDetails
@@ -32,7 +64,12 @@ func ImageList(repositoryName string, registryId string, repositoryUri string) (
         pushedAt := v.ImagePushedAt
         size := v.ImageSizeInBytes
         tags := v.ImageTags
-        uri := fmt.Sprintf("%s:%s", repositoryUri, v.ImageTags[0])
+        // 使われているタグを検索
+        tag, terr := ImageTag(imageIds, tags, *digest)
+        if terr != nil {
+            return nil, fmt.Errorf("リポジトリ（%s）のイメージURIの取得に失敗しました : %s", repositoryName, terr)
+        }
+        uri := fmt.Sprintf("%s:%s", repositoryUri, *tag)
         image := Image{
             Digest: *digest,
             PushedAt: *pushedAt,

f8735f7db9ea2d8b5d0ee00348decb6c43df34fb
hmatsu47 <hmatsu47@gmail.com>
2022-09-06 06:08:57 +0000

    AWS ECR API リージョンを自動判別

--- api/ecr.go
+++ api/ecr.go
@@ -3,6 +3,7 @@ package api
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
@@ -11,80 +12,80 @@ import (
 
 // 対象のイメージタグを検索
 func ImageTag(ids []types.ImageIdentifier, tags []string, digest string) string {
-    for _, id := range ids {
-        for _, tag := range tags {
-            // nil pointer 回避
-            if id.ImageTag != nil {
-                // 値の比較を可能にするために変数に代入
-                vsTag := *id.ImageTag
-                vsDigest := *id.ImageDigest
-                if vsTag == tag && vsDigest == digest {
-                    return tag
-                }
-            }
-        }
-    }
-    return ""
+	for _, id := range ids {
+		for _, tag := range tags {
+			// nil pointer 回避
+			if id.ImageTag != nil {
+				// 値の比較を可能にするために変数に代入
+				vsTag := *id.ImageTag
+				vsDigest := *id.ImageDigest
+				if vsTag == tag && vsDigest == digest {
+					return tag
+				}
+			}
+		}
+	}
+	return ""
 }
 
 // ECR リポジトリ内イメージ一覧取得
 func ImageList(repositoryName string, registryId string, repositoryUri string) ([]Image, error) {
-    cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-northeast-1"))
-    if err != nil {
-        return nil, fmt.Errorf("AWS（API）の認証に失敗しました : %s", err)
-    }
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(strings.Split(repositoryUri, ".")[3]))
+	if err != nil {
+		return nil, fmt.Errorf("AWS（API）の認証に失敗しました : %s", err)
+	}
 
-    // ページネーションさせないために最大件数を 1,000 に（実際には数十個程度の想定）
-    maxResults := int32(1000)
-    ecrImagesClient := ecr.NewFromConfig(cfg)
+	// ページネーションさせないために最大件数を 1,000 に（実際には数十個程度の想定）
+	maxResults := int32(1000)
+	ecrImagesClient := ecr.NewFromConfig(cfg)
 
-    // 一旦イメージ一覧を取得しておく（URI の一部としてどのタグを使っているのかを後で検索する）
-    ecrImageIds, ierr := ecrImagesClient.ListImages(context.TODO(), &ecr.ListImagesInput{
-        RepositoryName: &repositoryName,
-        RegistryId: &registryId,
-        MaxResults: &maxResults,
-    })
-    if ierr != nil {
-        return nil, fmt.Errorf("リポジトリ（%s）のイメージ一覧の取得に失敗しました : %s", repositoryName, ierr)
-    }
-    imageIds := ecrImageIds.ImageIds
+	// 一旦イメージ一覧を取得しておく（URI の一部としてどのタグを使っているのかを後で検索する）
+	ecrImageIds, ierr := ecrImagesClient.ListImages(context.TODO(), &ecr.ListImagesInput{
+		RepositoryName: &repositoryName,
+		RegistryId:     &registryId,
+		MaxResults:     &maxResults,
+	})
+	if ierr != nil {
+		return nil, fmt.Errorf("リポジトリ（%s）のイメージ一覧の取得に失敗しました : %s", repositoryName, ierr)
+	}
+	imageIds := ecrImageIds.ImageIds
 
-    // イメージ詳細一覧を取得
-    ecrImages, eerr := ecrImagesClient.DescribeImages(context.TODO(), &ecr.DescribeImagesInput{
-        RepositoryName: &repositoryName,
-        RegistryId: &registryId,
-        MaxResults: &maxResults,
-    })
-    if eerr != nil {
-        return nil, fmt.Errorf("リポジトリ（%s）のイメージ詳細一覧の取得に失敗しました : %s", repositoryName, eerr)
-    }
+	// イメージ詳細一覧を取得
+	ecrImages, eerr := ecrImagesClient.DescribeImages(context.TODO(), &ecr.DescribeImagesInput{
+		RepositoryName: &repositoryName,
+		RegistryId:     &registryId,
+		MaxResults:     &maxResults,
+	})
+	if eerr != nil {
+		return nil, fmt.Errorf("リポジトリ（%s）のイメージ詳細一覧の取得に失敗しました : %s", repositoryName, eerr)
+	}
 
-    imageDetails := ecrImages.ImageDetails
+	imageDetails := ecrImages.ImageDetails
 
-    var imageList []Image
-    for _, v := range imageDetails {
-        digest := v.ImageDigest
-        pushedAt := v.ImagePushedAt
-        size := v.ImageSizeInBytes
-        tags := v.ImageTags
-        // URI に使われているタグを検索
-        tag := ImageTag(imageIds, tags, *digest)
-        var uri string
-        if tag == "" {
-            // タグがない場合はダイジェスト
-            uri = fmt.Sprintf("%s@%s", repositoryUri, *digest)
-        } else {
-            uri = fmt.Sprintf("%s:%s", repositoryUri, tag)
-        }
-        image := Image{
-            Digest: *digest,
-            PushedAt: *pushedAt,
-            RepositoryName: repositoryName,
-            Size: float32(*size),
-            Tags: tags,
-            Uri: uri,
-        }
-        imageList = append(imageList, image)
-    }
-    return imageList, nil
+	var imageList []Image
+	for _, v := range imageDetails {
+		digest := v.ImageDigest
+		pushedAt := v.ImagePushedAt
+		size := v.ImageSizeInBytes
+		tags := v.ImageTags
+		// URI に使われているタグを検索
+		tag := ImageTag(imageIds, tags, *digest)
+		var uri string
+		if tag == "" {
+			// タグがない場合はダイジェスト
+			uri = fmt.Sprintf("%s@%s", repositoryUri, *digest)
+		} else {
+			uri = fmt.Sprintf("%s:%s", repositoryUri, tag)
+		}
+		image := Image{
+			Digest:         *digest,
+			PushedAt:       *pushedAt,
+			RepositoryName: repositoryName,
+			Size:           float32(*size),
+			Tags:           tags,
+			Uri:            uri,
+		}
+		imageList = append(imageList, image)
+	}
+	return imageList, nil
 }

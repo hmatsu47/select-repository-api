34a95bdff10e3ab039bcb2ca8924239a35658269
hmatsu47 <hmatsu47@gmail.com>
2023-04-05 11:14:12 +0900

    一覧取得微修正

--- api/ecr.go
+++ api/ecr.go
@@ -114,8 +114,6 @@ func GetImageList(imageIds []types.ImageIdentifier, imageDetails []types.ImageDe
 
 // ECR リポジトリ内イメージ一覧取得
 func ImageList(ctx context.Context, api *ecr.Client, repositoryName string, registryId string, repositoryUri string) ([]Image, error) {
-	var err error
-
 	imageIds, err := EcrListImages(context.TODO(), api, repositoryName, registryId)
 	if err != nil {
 		return nil, err
